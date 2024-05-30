b656b1bac17907f8453373c7b645348bf3bd5eb3
Kyrylo Belichenko <137775410+1x1NEBULAR1x1@users.noreply.github.com>
2024-05-30 23:35:20 +0200

    Update heandlers.py

--- heandlers.py
+++ heandlers.py
@@ -1,8 +1,6 @@
 import asyncio
 import datetime
 import re
-
-import requests
 from aiogram import Router, Bot, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
