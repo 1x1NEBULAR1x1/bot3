743172b3f12077fea051e68d58df6e2b5555892d
Kyrylo Belichenko <137775410+1x1NEBULAR1x1@users.noreply.github.com>
2024-05-30 23:19:31 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,6 +14,10 @@ import uvicorn
 
 app = FastAPI()
 
+@app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
 async def api():
     config = uvicorn.Config(host='0.0.0.0', port=8000, app=app)
     server = uvicorn.Server(config=config)
